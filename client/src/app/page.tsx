9e70749814f91ea3d62bbb7fc789e3d9a0e325e1
Abish-x-lethal <magarabish456@gmail.com>
2023-06-18 20:10:32 +0545

    room code left

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,25 +1,40 @@
-"use client"
-import { useEffect, useState } from "react"
-import io from "socket.io-client"
-const socket = io("http://localhost:3001")
-export default function Home() {
-  const [ msg,setMsg] = useState("");
-  const [messageReceived,setMessageReceived] = useState("");
-  const sendMsg=()=>{
-    socket.emit("send_msg",msg)
 
-  }
-useEffect(() => {
-  socket.on("receive_msg", (data:string) => {
-    setMessageReceived(data);
-  });
-}, [socket]);
+"use client"
+import { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+const socket = io("http://localhost:3001");
+
+export default function Home() {
+  const [msg, setMsg] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const sendMsg = () => {
+    socket.emit("send_msg", msg);
+    setMessages((prevMessages) => [...prevMessages, `You: ${msg}`]);
+    setMsg("");
+  };
+
+  useEffect(() => {
+    socket.on("receive_msg", (data: string) => {
+      setMessages((prevMessages) => [...prevMessages, `Received: ${data}`]);
+    });
+  }, []);
+
   return (
- <div>
-  <input placeholder='Message' onChange={(event)=>setMsg(event.target.value)}></input>
-  <button onClick={sendMsg}>Send Message</button>
-  <h1>MESSAGE</h1>
-  {messageReceived}
- </div>
-  )
+    <div>
+      <input
+        placeholder="Message"
+        value={msg}
+        onChange={(event) => setMsg(event.target.value)}
+      ></input>
+      <button onClick={sendMsg}>Send Message</button>
+      <h1>MESSAGE</h1>
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+      </div>
+    </div>
+  );
 }
