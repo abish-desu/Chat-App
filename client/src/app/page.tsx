3f706e1b78ceedc13934be01e32b781ac4f9f378
Abish-x-lethal <magarabish456@gmail.com>
2023-06-18 20:49:16 +0545

    room code update

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,3 @@
-
 "use client"
 import { useEffect, useState } from "react";
 import io from "socket.io-client";
@@ -8,33 +7,58 @@ const socket = io("http://localhost:3001");
 export default function Home() {
   const [msg, setMsg] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
+  const [room, setRoom] = useState("");
 
   const sendMsg = () => {
-    socket.emit("send_msg", msg);
+    socket.emit("send_msg", { room, msg });
     setMessages((prevMessages) => [...prevMessages, `You: ${msg}`]);
     setMsg("");
   };
 
   useEffect(() => {
-    socket.on("receive_msg", (data: string) => {
-      setMessages((prevMessages) => [...prevMessages, `Received: ${data}`]);
+    socket.on("receive_msg", (data: { room: string, message: string }) => {
+      setMessages((prevMessages) => [...prevMessages, `Received (${data.room}): ${data.message}`]);
     });
   }, []);
 
+  const joinRoom = () => {
+    socket.emit("join_room", room);
+  };
+
   return (
-    <div>
-      <input
-        placeholder="Message"
-        value={msg}
-        onChange={(event) => setMsg(event.target.value)}
-      ></input>
-      <button onClick={sendMsg}>Send Message</button>
-      <h1>MESSAGE</h1>
+    <div className="flex flex-col items-center  mt-4">
       <div>
-        {messages.map((message, index) => (
-          <p key={index}>{message}</p>
-        ))}
-      </div>
-    </div>
-  );
+      <input
+    className="outline-none border border-black p-2 mb-4 rounded-3xl bg-transparent w-[400px] text-black pl-4 pr-4"
+    placeholder="Room"
+    value={room}
+    onChange={(event) => setRoom(event.target.value)}
+    /
+  >
+  <button onClick={joinRoom}
+              className="outline-none border border-black p-2 mb-4 rounded-3xl bg-transparent ml-2 text-black px-7"
+
+  >Join Room</button>
+  </div>
+  <div>
+  <input
+    placeholder="Message"
+    className="outline-none border border-black p-2 mb-4 rounded-3xl bg-transparent w-[400px] text-black pl-4 pr-4"
+
+    value={msg}
+    onChange={(event) => setMsg(event.target.value)}
+  ></input>
+  <button onClick={sendMsg}
+                className="outline-none border border-black p-2 mb-4 rounded-3xl bg-transparent ml-2 text-black pl-4 pr-4"
+                >Send Message</button>
+  </div>
+  <h1>YOUR MESSAGES</h1>
+  <div>
+    {messages.map((message, index) => (
+      <p key={index}>{message}</p>
+    ))}
+  </div>
+</div>
+);
 }
+      
--- server/index.js
+++ server/index.js
@@ -1,23 +1,43 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const {Server} = require("socket.io");
+const { Server } = require("socket.io");
 const cors = require("cors");
 app.use(cors());
 const server = http.createServer(app);
 
-const io = new Server(server,{
-    cors:{
-        origin: "http://localhost:3000",
-        methods:["GET","POST"]
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
+
+const rooms = new Map();
+
+io.on("connection", (socket) => {
+  console.log(`user connected: ${socket.id}`);
+
+  socket.on("join_room", (room) => {
+    socket.join(room);
+    console.log(`User ${socket.id} joined room ${room}`);
+    socket.room = room;
+  });
+
+  socket.on("send_msg", (data) => {
+    const { room, msg } = data;
+    socket.to(room).emit("receive_msg", { room, message: msg });
+  });
+
+  socket.on("disconnect", () => {
+    console.log(`user disconnected: ${socket.id}`);
+    if (socket.room) {
+      socket.leave(socket.room);
+      console.log(`User ${socket.id} left room ${socket.room}`);
     }
-})
-io.on("connection",(socket)=>{
-    console.log(`user connected : ${socket.id}`)
-    socket.on("send_msg", (data)=>{
-        socket.broadcast.emit("receive_msg",data)
-    })
-})
-server.listen(3001,()=>{
-    console.log("server is running")
+  });
+});
+
+server.listen(3001, () => {
+  console.log("server is running");
 });
